8e5aeb1b7560966a3708d56ae83f3aa4b8ff8dc4
4334n <charles.cus.77@gmail.com>
2025-04-17 13:11:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ if __name__ == "__main__":
     read_and_send_dht_data()
     # toggle_led()
     # run_stream_server()
+
+from sensors.dht import read_and_send_dht_data
+from control.led_control import toggle_led
+from camera.stream import run_stream_server
+
+if __name__ == "__main__":
+    read_and_send_dht_data()
+    # toggle_led()
+    # run_stream_server()
